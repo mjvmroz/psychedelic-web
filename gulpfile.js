2126845ae2971de10818720d000bfb3551e4a94a
Michael Mroz <mmroz@atlassian.com>
2016-02-21 22:25:15 +1100

    Updated build-chain so it like... works

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,86 @@
 'use strict';
 
-/* Sourced from: https://gist.github.com/TravelingTechGuy/9996733 */
-
 const gulp = require('gulp'),
-	clean = require('gulp-clean'),
-	cleanhtml = require('gulp-cleanhtml'),
-	minifycss = require('gulp-minify-css'),
-	jshint = require('gulp-jshint'),
-	stripdebug = require('gulp-strip-debug'),
-	uglify = require('gulp-uglify'),
-	zip = require('gulp-zip');
+  clean = require('gulp-clean'),
+  cleanhtml = require('gulp-cleanhtml'),
+  minifycss = require('gulp-minify-css'),
+  jshint = require('gulp-jshint'),
+  stripdebug = require('gulp-strip-debug'),
+  uglify = require('gulp-uglify'),
+  gutil = require('gutil'),
+  zip = require('gulp-zip'),
+
+  createStyleTask = function(path) {
+    // return gulp.src('src/styles/**/*.css')
+    //   .pipe(minifycss({root: 'src/styles', keepSpecialComments: 0}))
+    //   .pipe(gulp.dest('build/styles'));
+    return gulp.src(`src/${path}**`)
+      .pipe(gulp.dest(`build/${path}`));
+  },
+  createJsHintTask = function(path) {
+    return gulp.src(`src/${path}*.js`)
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'));
+  },
+  createScriptTask = function(path) {
+    return gulp.src([`src/${path}/**/*.js`])
+      .pipe(stripdebug())
+      .pipe(uglify({}))
+      .pipe(gulp.dest(`build/${path}`));
+  },
+
+  copyDirectories = ['assets/fonts/', 'icons/', '_locales/'],
+  jsDependencies = [];
 
 //clean build directory
 gulp.task('clean', () => {
-	return gulp.src('build/*', {read: false})
-		.pipe(clean());
+  return gulp.src('build/*', {read: false})
+    .pipe(clean());
 });
 
 //copy static folders to build directory
 gulp.task('copy', () => {
-	gulp.src('src/fonts/**')
-		.pipe(gulp.dest('build/fonts'));
-	gulp.src('src/icons/**')
-		.pipe(gulp.dest('build/icons'));
-	gulp.src('src/_locales/**')
-		.pipe(gulp.dest('build/_locales'));
-	return gulp.src('src/manifest.json')
-		.pipe(gulp.dest('build'));
+  return copyDirectories.map((dir, i) => {
+    gulp.src(`src/${dir}**`)
+      .pipe(gulp.dest(`build/${dir}`));
+  }).concat([
+    gulp.src('src/manifest.json').pipe(gulp.dest('build/'))
+  ]);
 });
 
 //copy and compress HTML files
 gulp.task('html', () => {
-	return gulp.src('src/*.html')
-		.pipe(cleanhtml())
-		.pipe(gulp.dest('build'));
+  return gulp.src('src/assets/html/*.html')
+    .pipe(cleanhtml())
+    .pipe(gulp.dest('build/'));
 });
 
 //run scripts through JSHint
-gulp.task('jshint', () => {
-	return gulp.src('src/scripts/*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
-});
+gulp.task('jshint', () => [
+    createJsHintTask('assets/js/'),
+    createJsHintTask('content_scripts/js/')]);
 
 //copy vendor scripts and uglify all other scripts, creating source maps
-gulp.task('scripts', ['jshint'], () => {
-	gulp.src('src/scripts/vendors/**/*.js')
-		.pipe(gulp.dest('build/scripts/vendors'));
-	return gulp.src(['src/scripts/**/*.js', '!src/scripts/vendors/**/*.js'])
-		.pipe(stripdebug())
-		.pipe(uglify({outSourceMap: true}))
-		.pipe(gulp.dest('build/scripts'));
-});
+gulp.task('scripts', ['jshint'], () => [
+    gulp.src(jsDependencies)
+      .pipe(gulp.dest('build/js/vendors')),
+    createScriptTask('assets/js/'),
+    createScriptTask('content_scripts/js/')]);
 
 //minify styles
-gulp.task('styles', () => {
-// 	return gulp.src('src/styles/**/*.css')
-// 		.pipe(minifycss({root: 'src/styles', keepSpecialComments: 0}))
-// 		.pipe(gulp.dest('build/styles'));
-	return gulp.src('src/styles/**')
-		.pipe(gulp.dest('build/styles'));
-});
+gulp.task('styles', () => [
+    createStyleTask('assets/css/'),
+    createStyleTask('content_scripts/css/')]);
 
 //build ditributable and sourcemaps after other tasks completed
 gulp.task('zip', ['html', 'scripts', 'styles', 'copy'], () => {
-	var manifest = require('./src/manifest'),
-		distFileName = manifest.name + ' v' + manifest.version + '.zip',
-		mapFileName = manifest.name + ' v' + manifest.version + '-maps.zip';
-	//collect all source maps
-	gulp.src('build/scripts/**/*.map')
-		.pipe(zip(mapFileName))
-		.pipe(gulp.dest('dist'));
-	//build distributable extension
-	return gulp.src(['build/**', '!build/scripts/**/*.map'])
-		.pipe(zip(distFileName))
-		.pipe(gulp.dest('dist'));
+  const manifest = require('./src/manifest'),
+    distFileName = `${manifest.name} v${manifest.version}.zip`,
+    mapFileName = manifest.name + ' v' + manifest.version + '-maps.zip';
+  //build distributable extension
+  return gulp.src(['build/**', '!build/js/**/*.map'])
+    .pipe(zip(distFileName))
+    .pipe(gulp.dest('dist'));
 });
 
 //run all tasks after build directory has been cleaned
